df7cb852243d5db98a043644355a9e08b40be95c
Hobr <mail@hobr.site>
2023-07-16 15:00:06 +0800

    source

--- modules/nix.nix
+++ modules/nix.nix
@@ -7,6 +7,7 @@
     settings = {
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
       auto-optimise-store = true;
+      substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
     };
   };
 }
