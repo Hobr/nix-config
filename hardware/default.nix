9695e48f789869a0730b577e7ff09854d55f48ab
Hobr <mail@hobr.site>
2023-07-16 15:00:14 +0800

    nvidia

--- hardware/default.nix
+++ hardware/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./laptop.nix
+    ./nvidia.nix
   ];
 }
---
+++ hardware/nvidia.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+{
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "nvidia-x11"
+      "nvidia-settings"
+    ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
