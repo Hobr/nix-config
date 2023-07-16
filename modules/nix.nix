87b68c8409868fc677f57b7993fd3aa6b53983ac
Hobr <mail@hobr.site>
2023-07-16 15:43:30 +0800

    system

--- modules/nix.nix
+++ modules/nix.nix
@@ -3,11 +3,23 @@
 {
   nix = {
     package = pkgs.nixFlakes;
-
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 3d";
+    };
     settings = {
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
       auto-optimise-store = true;
-      substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
+      substituters = lib.mkForce [
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        "https://mirrors.cernet.edu.cn/nix-channels/store"
+        "https://mirrors.bfsu.edu.cn/nix-channels/store"
+        "${rp}https://cache.nixos.org"
+        "${rp}https://rewine.cachix.org"
+        "${rp}https://xddxdd.cachix.org"
+        "${rp}https://cache.garnix.io"
+      ];
     };
   };
 }
